215b1df1c6207a1e90fa9342c202c9476181cb85
Marcin Widz <Marcinwidz91@gmail.com>
2021-09-29 20:14:05 +0200

    save and email bux fixes

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import "./Home.css";
 import axios from "axios";
 import FormUserFront from "../../Components/FormUserFront/FormUserFront";
-<<<<<<< HEAD
 import loading from "../../assets/loading.json";
-=======
-import Loading from "../../Components/Loading";
->>>>>>> 2fa0aafbc25f77353585bd476fd7cf32cdaf75d7
 
 function Home() {
   const [isLoading, setisLoading] = useState(true);
@@ -31,7 +27,6 @@ function Home() {
     fetchData();
   }, []);
   return isLoading ? (
-<<<<<<< HEAD
     <div>
       <script src='https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js'></script>
       <lottie-player
@@ -43,9 +38,6 @@ function Home() {
         autoplay
       ></lottie-player>
     </div>
-=======
-    <Loading />
->>>>>>> 2fa0aafbc25f77353585bd476fd7cf32cdaf75d7
   ) : (
     <div className='home-container'>
       <h1>Choisissez le questionnaire à prendre:</h1>
